3d98712799d6a12bcc4666b19791eef35107ea80
Andres Rivero <arivero@MacBookPro.local>
2018-09-24 22:54:09 -0700

    added the news sources for each individual news source

--- src/app/news-source/news-source.component.ts
+++ src/app/news-source/news-source.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-news-source',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsSourceComponent implements OnInit {
 
-  constructor() { }
+  inProgress:boolean = true;
+  sourceData:Object = null;
+  sourceId:string;
+
+  constructor(private http:HttpService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe( params => {
+        this.sourceId = params['id'];
+        this.http.getTopHeadlines({'sources': this.sourceId})
+          .subscribe(data => {
+            console.log("Data", data),
+            this.sourceData = data;
+          },
+          err => {
+            console.error(err)
+          },
+          () => this.inProgress = false
+        )
+      });
   }
 
 }
